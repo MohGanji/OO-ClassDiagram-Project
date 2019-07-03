0ecaa71f296b4df76df159c591f8026bc17a8b16
Vahid Mohammadi <vahidm1467@gmail.com>
2019-07-03 23:16:31 +0430

    fixes

--- implementation/src/masoul-amoozesh.ts
+++
@@ -1,7 +0,0 @@
-import Person from './person';
-
-export default class MasoulAmoozesh extends Person {
-  constructor(firstName: string, lastName: string, phoneNumber: string, nationalCode: string) {
-    super(firstName, lastName, phoneNumber, nationalCode);
-  }
-}
