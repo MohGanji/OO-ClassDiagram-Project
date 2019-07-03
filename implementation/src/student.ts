cb553d9fb0821526cc1381f73b91e255ad8de53a
Mohammad macbook <mfg1376@gmail.com>
2019-07-04 01:15:19 +0430

    add chart implementation and graduationHandler

--- implementation/src/student.ts
+++ implementation/src/student.ts
@@ -3,18 +3,25 @@ import EducationalTerm from './educational-term';
 import EntekhabVahedTerm from './entekhab-vahed-term';
 import Person from './person';
 import StudentRepository from './repositories/student-repository';
+import GraduationHandler from './graduation-handler';
+import { Chart } from './chart';
+import { Course } from './course-agg/course';
 
 export default interface Student {
   readonly sid: string;
   readonly vahedPassed: number;
   readonly avgGrade: number;
   readonly enteranceTerm: EducationalTerm;
+  readonly chart: Chart;
   getAllPassedCourses(): CourseTermi[];
   addEntekhabVahedTerm(entekhabVahedTerm: EntekhabVahedTerm): void;
   getCurrentTermEntekhabVahed(): EntekhabVahedTerm | undefined;
 }
 
 class StudentImpl implements Student {
+  public get chart() {
+    return this._chart
+  }
   public get sid(): string {
     return this._sid;
   }
@@ -29,7 +36,7 @@ class StudentImpl implements Student {
   get avgGrade() {
     return this._avgGrade;
   }
-  constructor(private person: Person, private _sid: string, private _enteranceTerm: EducationalTerm) {}
+  constructor(private person: Person, private _sid: string, private _enteranceTerm: EducationalTerm, private _chart: Chart) { }
   addEntekhabVahedTerm(entekhabVahedTerm: EntekhabVahedTerm) {
     this.entekhabVahedTerms.push(entekhabVahedTerm);
   }
@@ -39,11 +46,18 @@ class StudentImpl implements Student {
   getCurrentTermEntekhabVahed(): EntekhabVahedTerm | undefined {
     return this.entekhabVahedTerms[this.entekhabVahedTerms.length - 1];
   }
+  hasGraduated(): boolean {
+    // TODO:
+    const myPassedCourses: Course[] = this.getAllPassedCourses().map(c => c.course);
+    return GraduationHandler.hasPassedRequiredCourses(this.chart, myPassedCourses)
+      && GraduationHandler.hasPassedEnoughOptionalCourses(this.chart, myPassedCourses)
+      && GraduationHandler.hasPassedSelectiveCourses(this.chart, myPassedCourses)
+  }
 }
 
 export const StudentFactory = {
-  createStudent(person: Person, sid: string, enteranceTerm: EducationalTerm) {
-    const student = new StudentImpl(person, sid, enteranceTerm);
+  createStudent(person: Person, sid: string, enteranceTerm: EducationalTerm, chart: Chart) {
+    const student = new StudentImpl(person, sid, enteranceTerm, chart);
     StudentRepository.save(student);
     return student;
   }
